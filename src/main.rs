658e7a2f91052a4be75b81f09f88e97f593c178a
Théo Huchard <theo.huchard@gmail.com>
2020-01-26 17:07:20 +0100

    Modified gitignore to take into account Cargo.lock. Added Clap to get my input and output commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("mypipe")
+        .version("1.0")
+        .about("Pipe program")
+        .author("Théo H")
+        .arg(
+            Arg::with_name("in")
+                .short("i")
+                .long("in")
+                .value_name("INPUT")
+                .help("INPUT CMD")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("out")
+                .short("o")
+                .long("out")
+                .value_name("OUTPUT")
+                .help("OUTPUT CMD")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let input_cmd = match matches.value_of("in"){
+        Some(x) => x,
+        None => panic!("No input command given")
+    };
+    let output_cmd = match matches.value_of("out"){
+        Some(x) => x,
+        None => panic!("No output command given")
+    };
+
+    println!("{:?}\n{:?}", input_cmd, output_cmd);
 }
